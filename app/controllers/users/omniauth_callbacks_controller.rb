6e701b097840845726705c4fb5630d48ca45a9aa
NunTheeKnee <jane6677doe@gmail.com>
2024-09-05 12:34:01 +0530

    Added the required feature

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -9,13 +9,22 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   # end
 
   def google_oauth2
-    user = User.from_omniauth(auth)
-    if user.present?
-      flash[:success] = t "devise.omniauth_callbacks.success", kind: "Google"
-      sign_in_and_redirect user, event: :authentication
+    if user_signed_in?
+      if current_user.email == auth.info.email
+        user = User.from_omniauth(auth)
+        redirect_to root_path, notice: "Avatar URL and Google ID is updated"
+      else
+        redirect_to root_path, alert: "Email are not matching"
+      end
     else
-      flash[:alert] = t "devise.omniauth_callbacks.failure", kind: "Google", reason: "User does not exist. Kindly Sign Up"
-      redirect_to new_user_registration_path(email: auth.info.email)
+      user = User.from_omniauth(auth)
+      if user.present?
+        flash[:success] = t "devise.omniauth_callbacks.success", kind: "Google"
+        sign_in_and_redirect user, event: :authentication
+      else
+        flash[:alert] = t "devise.omniauth_callbacks.failure", kind: "Google", reason: "User does not exist. Kindly Sign Up"
+        redirect_to new_user_registration_path(email: auth.info.email)
+      end
     end
   end
   private
