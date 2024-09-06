ec80e64b1b114e04111b0bb9749f94e9aefb5340
NunTheeKnee <jane6677doe@gmail.com>
2024-09-06 13:51:43 +0530

    Segregated the functions and changed function into meaningful names

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,31 +1,11 @@
-# frozen_string_literal: true
-
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   skip_before_action :verify_authenticity_token, only: [:google_oauth2]
-  # You should configure your model like this:
-  # devise :omniauthable, omniauth_providers: [:twitter]
-
-  # You should also create an action method in this controller like this:
-  # def twitter
-  # end
-
+  #Callback after redirection from google and returns the value fetched from the user google account
   def google_oauth2
     if user_signed_in?
-      if current_user.email == auth.info.email
-        user = User.from_omniauth(auth)
-        redirect_to root_path, notice: "Avatar URL and Google ID is updated"
-      else
-        redirect_to root_path, alert: "Emails are not matching"
-      end
+      connect_with_google
     else
-      user = User.from_omniauth(auth)
-      if user.present?
-        flash[:success] = t "devise.omniauth_callbacks.success", kind: "Google"
-        sign_in_and_redirect user, event: :authentication
-      else
-        flash[:alert] = t "devise.omniauth_callbacks.failure", kind: "Google", reason: "User does not exist. Kindly Sign Up"
-        redirect_to new_user_registration_path(email: auth.info.email)
-      end
+      register_user_google
     end
   end
 
@@ -33,5 +13,32 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def auth
     @auth ||= request.env['omniauth.auth']
+  end
+
+  #This function connects the user with the google
+  def connect_with_google
+    if current_user.email == auth.info.email
+      user = User.from_omniauth(auth)
+      if user.present?
+        User.update_user_credentials(auth)
+        redirect_to root_path, notice: "User is connected with google"
+      else
+        redirect_to root_path, alert: "User doesn't exist"
+      end
+    else
+      redirect_to root_path, alert: "Use your registered Labsmart email for google login"
+    end
+  end
+
+  #This function redirects the user to registration page if the user is not registered but tries accessing with google
+  def register_user_google
+    user = User.from_omniauth(auth)
+    if user.present?
+      flash[:success] = t "devise.omniauth_callbacks.success", kind: "Google"
+      sign_in_and_redirect user, event: :authentication
+    else
+      flash[:alert] = t "devise.omniauth_callbacks.failure", kind: "Google", reason: "User does not exist. Kindly Sign Up"
+      redirect_to new_user_registration_path(email: auth.info.email)
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,18 +5,27 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: %i[google_oauth2]
 
+  #This function checks if the user provided google email exists or not
   def self.from_omniauth(auth)
     user = where(email: auth.info.email).first do |u|
       u.google_auth_uid = auth.uid
     end
+    if user.present?
+      user
+    else
+      nil
+    end
+  end
 
+  #This function updates the user avatar_url and google_auth_id
+  def self.update_user_credentials(auth)
+    user = where(email: auth.info.email).first do |u|
+      u.google_auth_uid = auth.uid
+    end
     if user.present?
       user.avatar_url = auth.info.image
       user.google_auth_uid = auth.uid
       user.save
-      user
-    else
-      nil
     end
   end
 end
